dbeda911bd96479fb5796c8404a5958bb11cb2d1
Jin Wu <jinn.wuu@gmail.com>
2020-02-24 17:29:22 -0500

    Done.

---
+++ js/index.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const searchForm = document.querySelector("#github-form")
+    const userList = document.querySelector("#user-list")
+
+    searchForm.addEventListener("submit", event => {
+        event.preventDefault()
+        const search = event.target.search.value
+
+        fetch(`https://api.github.com/search/users?q=${search}`)
+            .then(response => response.json())
+            .then(result => {
+                result.items.forEach(user => {
+                    renderUser(user)
+                })
+            })
+    })
+
+    function renderUser(user) {
+        const newLi = document.createElement("li")
+        newLi.innerHTML = `
+            <h3>${user.login}</h3>
+            <img src="${user.avatar_url}"/><br>
+            <a href="${user.html_url}">${user.login}'s repo link</a>
+        `
+        userList.append(newLi)
+    }
+
+})
