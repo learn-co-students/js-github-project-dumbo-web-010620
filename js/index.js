c1485f1b9a3715f939f8f1904d4d237edbe50c49
wenglin1 <wenglin@buffalo.edu>
2020-02-26 01:27:19 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,29 +1,43 @@
+// load the document
 document.addEventListener("DOMContentLoaded", () => {
-
+    
+    // github-form search element
     const searchForm = document.getElementById("github-form")
-    const userList = document.getElementById("user-list") 
+    // user-list
+    const userList = document.getElementById("user-list")
 
-    searchForm.addEventListener("submit", e => {
-        e.preventDefault()
-        const search = e.target.search.value
-
+    searchForm.addEventListener("submit", event => {
+        event.preventDefault()
+        const search = event.target.search.value
+        
+        // fetch the api
         fetch(`https://api.github.com/search/users?q=${search}`)
-            .then(response => response.json())
-            .then(result => {
-                result.items.forEach(user => {
-                    renderUser(user)
-                })
+        // take event and fetch json
+        .then(response => response.json())
+        // turn the json into legible data
+        .then(result => {
+            //iterate over each user with username
+            result.items.forEach(user => {
+                // renderUser function 
+                renderUser(user)
             })
-        e.target.reset
+        })
+
+        // reset the target with blank
+        event.target.reset()
     })
 
+    // displays user data
     function renderUser(user) {
+        // creating a new li element so we can append later
         const newLi = document.createElement("li")
+        // html format for displaying user data
         newLi.innerHTML = `
             <h3>${user.login}</h3>
             <img src="${user.avatar_url}"/><br>
             <a href="${user.html_url}">${user.login}'s repo link</a>
         `
+        // appending result to our list
         userList.append(newLi)
     }
-})  
+})
